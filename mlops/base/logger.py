42aba698f33870bc93d95b2173e8483c66c38b6a
godxvincent <godxvincent@gmail.com>
2025-10-28 22:36:59 -0600

    :bug: Fixing bug with Logger

--- mlops/base/logger.py
+++ mlops/base/logger.py
@@ -53,10 +53,10 @@ class BaseLogger:
             def wrapper(*args, **kwargs):
                 self.logger.debug(f"function {func.__name__} was called with args: {args} and kwargs: {kwargs} ")
                 result = func(*args, **kwargs)
-                # if result:
-                #     self.logger.debug(f"function {func.__name__} returned: {result}")
-                # else:
-                self.logger.debug(f"function {func.__name__} finished correctly")
+                if type(result) != type(None) :
+                    self.logger.debug(f"function {func.__name__} returned: {result}")
+                else:
+                    self.logger.debug(f"function {func.__name__} finished correctly")
                 return result
             return wrapper
 
