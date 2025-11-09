c37efe1f962b60cf71316cd4d29d11830710d1ab
godxvincent <godxvincent@gmail.com>
2025-11-09 11:02:32 -0600

    :construction: Explicacion

---
+++ tests/dataset_test.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+
+from mlops.dataset import DataLoader
+from mlops.modeling.constants import EXPECTED_SCHEMA
+
+
+@pytest.fixture(autouse=True)
+def setup_data():
+    # Code to run before each test
+    print("\nSetting up before test")
+    os.environ["ENV_FOR_DYNACONF"] = "testing"
+    yield
+    # Code to run after each test (teardown)
+    os.environ.pop("ENV_FOR_DYNACONF", None)
+    print("\nTearing down after test")
+
+
+def test_dataloader_class_get_shape():
+    dl_class = DataLoader()
+    dl_class.load_file("turkish_music_emotion_modified.csv")
+    print(EXPECTED_SCHEMA)
+    (filas, columnas) = dl_class.get_shape()
+    assert filas == 408
+    assert columnas == 52
